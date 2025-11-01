4d2d152e0907d3c14c6041e141c423cebca18c2a
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-10-31 21:43:42 -0300

    Add interface que serao usadas no projeto alem dasmigrates

---
+++ backend/prisma/migrations/20251101003552_test_user/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiry" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
