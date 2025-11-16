ea4aaf2cbfd79fd6a6ad11ff276e2ae73fd8e3d0
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-11-16 15:41:29 -0300

    feat: Fiz a inclusão de migrate Questions

---
+++ backend/prisma/migrations/20251116183928_add_question_relation/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "questions" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "ativo" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "questions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "questions" ADD CONSTRAINT "questions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
